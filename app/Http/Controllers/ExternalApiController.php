1245e5f999f1d55ddfa04c37b63958f37a55f8d1
adilsoncruzjr <adilsoncruz.jr@gmail.com>
2024-09-08 18:48:51 -0300

    Construction external route

--- app/Http/Controllers/ExternalApiController.php
+++ app/Http/Controllers/ExternalApiController.php
@@ -5,7 +5,26 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Http;
+
 class ExternalApiController extends Controller
 {
-    //
+    public function getData()
+    {
+        $apiUrl = env('EXTERNAL_API_URL');
+        $apiKey = env('EXTERNAL_API_KEY');
+
+        $response = Http::withHeaders([
+            'x_cg_demo_api_key' => $apiKey,
+        ])->get($apiUrl);
+
+        if ($response->successful()) {
+            return response()->json($response->json());
+        }
+
+        return response()->json([
+            'error' => 'Erro ao consumir API externa',
+            'status' => $response->status()
+        ], 500);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+route::get('/external-api', [App\Http\Controllers\ExternalApiController::class, 'getData']);
