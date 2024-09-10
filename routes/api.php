2efcae41a05f41255b15c9f9005f3003a6341745
adilsoncruzjr <adilsoncruz.jr@gmail.com>
2024-09-10 18:29:08 -0300

    Route login and logout

--- routes/api.php
+++ routes/api.php
@@ -11,5 +11,5 @@ use App\Http\Controllers\AuthController;
 
 route::get('/external-api', [App\Http\Controllers\ExternalApiController::class, 'getData']);
 Route::post('/register', [AuthController::class, 'register']);
-//Route::post('/login', [AuthController::class, 'login']);
-//Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
